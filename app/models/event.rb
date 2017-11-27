9363bc0481e571ede27325d628502e872c7c5c2d
arthur reboul <arthur.reboul@gmail.com>
2017-11-27 15:14:30 +0100

    models finalized

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+
+  validates :title, presence: true
+  validates :date, presence: true
+  validates :location, presence: true
+
 end
---
+++ db/migrate/20171127140056_add_status_to_booking.rb
@@ -0,0 +1,5 @@
+class AddStatusToBooking < ActiveRecord::Migration[5.1]
+  def change
+    add_column :bookings, :status, :string
+  end
+end
