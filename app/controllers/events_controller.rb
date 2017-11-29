109a81390d0adb2f2e5b112f7d7d800487f2d2d7
oruhtra <31852619+oruhtra@users.noreply.github.com>
2017-11-29 15:07:06 +0100

    Update events_controller.rb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,11 +2,11 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
-    @users = User.where("status = ? AND id != ?", true, @event.user_id)
     @booking = Booking.new
     @bookings = @event.bookings
+    @iduser_booked = @bookings.map { |booking| booking.user.id }
+    @users = User.where(status: true).where.not(id: @iduser_booked.push(@event.user_id))
     authorize @event
-
     @events = Event.where.not(latitude: nil, longitude: nil)
 
     @markers = @events.map do |event|
