1859917b1b78623d867687758ed7e849eae36c78
hortense gilbert <hortensegilbert@hotmail.com>
2017-11-29 12:06:32 +0100

    adding bookings sweeper to Event Page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,9 +2,16 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
-    @users = User.where("status = ? AND id != ?", true, @event.user_id)
+    # @users = User.where("status = ? AND id != ?", true, @event.user_id)
     @booking = Booking.new
     @bookings = @event.bookings
+    @iduser_booked = @bookings.map { |booking| booking.user.id }
+
+    @users = User.where(status: true).where.not(id: @iduser_booked.push(@event.user_id))
+
+    # @bookings.each do |booking|
+    #   @users.reject { |item| item == booking.user }
+    # end
     authorize @event
   end
 
@@ -38,5 +45,6 @@ class EventsController < ApplicationController
   def event_params
     params.require(:event).permit(:title, :location, :date)
   end
+
 
 end
