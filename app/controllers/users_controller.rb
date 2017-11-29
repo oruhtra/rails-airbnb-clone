681f403d9a24c9c873a061e128b1f2fb8a715e83
oruhtra <31852619+oruhtra@users.noreply.github.com>
2017-11-29 15:08:19 +0100

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,9 @@ class UsersController < ApplicationController
   def show
     @events = @user.events
     @bookings = @user.bookings
+    @event = Event.find(params[:event_id]) if !params[:event_id].nil?
     authorize @user
-
+    
     @users = User.where.not(latitude: nil, longitude: nil)
 
     @markers = @users.map do |user|
