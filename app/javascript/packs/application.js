01bee3091bc64febd38f4618ddb8f806594a289a
celestin prudhomme <celestin.prudhomme@gmail.com>
2017-11-30 14:45:08 +0100

    audio player on homepage, création partial et js components

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,10 @@ import "../plugins/flatpickr";
 
 import { showField } from '../components/showField';
 import { loadDynamicBannerText } from '../components/banner';
-
+import { audioPlayer } from '../components/audioPlayer';
 showField();
 loadDynamicBannerText();
+audioPlayer();
 
 
 
